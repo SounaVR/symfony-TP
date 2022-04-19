5de93e91a544bbe01f67a40bd9b04ac16b8a43ec
SounaFR <sounafr@protonmail.com>
2022-04-19 16:37:23 +0200

    pagination

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,12 +13,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(ManagerRegistry $doctrine, PaginatorInterface $paginator, Request $request): Response
     {
         $repository = $doctrine->getRepository(Article::class);
-        $articles = $repository->findAll();
+
+        $pagination = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            2
+        );
         return $this->render('accueil/index.html.twig', [
-            'articles' => $articles
+            'pagination' => $pagination
         ]);
     }
 }
